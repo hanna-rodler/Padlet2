f8dc345900d9f02118377e8745a4acd899f8a828
hanna-rodler <haj.rodler@gmail.com>
2023-05-24 14:52:39 +0200

    move routes into middleware but not working yet

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\EntryController;
 use App\Http\Controllers\PadletController;
@@ -7,8 +9,6 @@ use App\Http\Controllers\RatingController;
 use App\Http\Controllers\RightController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,43 +29,50 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 //Route::get('/', [PadletController::class,  'publicList']);
+// Padlets
 Route::get('/publicPadlets', [PadletController::class,  'publicList']);
-// TODO: get all private padlets and padlets that user is inivted to
-Route::get('/privateAndInvited/{userId}', [PadletController::class, 'privateAndInvitedPadlets']);
-
 Route::get('/padlets/{id}', [PadletController::class, 'detail']);
-Route::put('/padlets/{id}', [PadletController::class, 'update']);
-Route::delete('/padlets/{id}', [PadletController::class, 'delete']);
 Route::post('/padlets', [PadletController::class, 'save']);
 
+// Entries
 Route::get('/entries/{id}', [EntryController::class, 'getSingle']);
-Route::put('/entries/{id}', [EntryController::class, 'update']);
-Route::delete('/entries/{id}', [EntryController::class, 'delete']);
-Route::post('/entries', [EntryController::class, 'save']);
 
-Route::get('/pendingInvitations/{userId}', [RightController::class, 'pendingInvitations']);
-Route::get('/nonPendingInvitations/{userId}', [RightController::class, 'nonPendingInvitations']);
-Route::post('/invite', [RightController::class, 'invite']);
-Route::delete('/invite', [RightController::class, 'deleteInvitation']);
-Route::put('/invitations/{userId}/{padletId}', [RightController::class, 'updateRight']);
-Route::put('/acceptInvitation/{userId}/{padletId}', [RightController::class,
-    'acceptInvitation']);
-Route::put('/declineInvitation/{userId}/{padletId}', [RightController::class,
-    'declineInvitation']);
-
-
-Route::post('/comments/{id}', [CommentController::class, 'save']);
-
-Route::get('/ratings/{entryId}/{userId}', [RatingController::class, 'ratingExists']);
-Route::post('/ratings', [RatingController::class, 'save']);
-
-
+// User
 Route::get('/users/{id}', [UserController::class, 'getUser']);
 Route::get('/users/mail/{mail}', [UserController::class, 'getUserByMail']);
 Route::post('/register', [UserController::class, 'register']);
 
 
-Route::group(['middleware' => ['api', 'auth.jwt']], function() {
-    Route::get('/padlets/private/{id}', [PadletController::class, 'privateList']);
+Route::group(['middleware'=>['api', 'auth.jwt']], function() {
+    // Authorization
     Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    // Padlets
+    Route::get('/padlets/private/{id}', [PadletController::class, 'privateList']);
+    Route::get('/privateAndInvited/{userId}', [PadletController::class, 'privateAndInvitedPadlets']);
+    Route::put('/padlets/{id}', [PadletController::class, 'update']);
+    Route::delete('/padlets/{id}', [PadletController::class, 'delete']);
+
+    // Entres
+    Route::put('/entries/{id}', [EntryController::class, 'update']);
+    Route::delete('/entries/{id}', [EntryController::class, 'delete']);
+    Route::post('/entries', [EntryController::class, 'save']);
+
+    // Rights
+    Route::get('/pendingInvitations/{userId}', [RightController::class, 'pendingInvitations']);
+    Route::get('/nonPendingInvitations/{userId}', [RightController::class, 'nonPendingInvitations']);
+    Route::put('/acceptInvitation/{userId}/{padletId}', [RightController::class,
+        'acceptInvitation']);
+    Route::put('/declineInvitation/{userId}/{padletId}', [RightController::class,
+        'declineInvitation']);
+    Route::put('/invitations/{userId}/{padletId}', [RightController::class, 'updateRight']);
+    Route::post('/invite', [RightController::class, 'invite']);
+    Route::delete('/invite', [RightController::class, 'deleteInvitation']);
+
+    // Ratings
+    Route::get('/ratings/{entryId}/{userId}', [RatingController::class, 'ratingExists']);
+    Route::post('/ratings', [RatingController::class, 'save']);
+
+    // Comments
+    Route::post('/comments/{id}', [CommentController::class, 'save']);
 });
